da66e3779a83d565259c3845ff5035142614e618
Tigerfire1 <164653634+Tigerfire1@users.noreply.github.com>
2026-08-27 02:20:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ const upload = multer({
 
 // Configure Nodemailer with environment variables from Render
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
